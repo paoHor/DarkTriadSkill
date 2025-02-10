a0ad53ec40b767bd444ef80f503b8011f4f820f6
paolaH <130796770+paoHor@users.noreply.github.com>
2025-02-10 09:32:55 +0100

    Add files via upload

---
+++ src/main/kotlin/furhatos/app/newskill/setting/interactionParams.kt
@@ -0,0 +1,5 @@
+package furhatos.app.newskill.setting
+
+/** Engagement parameters */
+const val MAX_NUMBER_OF_USERS = 5 // Max amount of people that Furhat will recognize as users simultaneously
+const val DISTANCE_TO_ENGAGE = 2.0 // Min distance for people to be recognised as users
